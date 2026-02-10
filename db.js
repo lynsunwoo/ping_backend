60d166f5011440212b86649a4284dc955fad8172
lynsunwoo <lynsunwoo@gmail.com>
2026-02-10 12:48:46 +0900

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 // db연결 설정 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'ping',
-  multipleStatements: true
+  host: process.env.DB_HOST,      // ⭐
+  user: process.env.DB_USER,      // ⭐
+  password: process.env.DB_PASSWORD, // ⭐
+  database: process.env.DB_NAME,  // ⭐
+  port: process.env.DB_PORT || 3306,
 });
 
 // 연결 
