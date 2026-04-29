606519325c0f7f52556277c47650f4d53d03631b
lynsunwoo <lynsunwoo@gmail.com>
2026-04-29 15:10:41 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,42 @@
 const mysql = require('mysql');
 
 // db연결 설정 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,      // ⭐
-  user: process.env.DB_USER,      // ⭐
-  password: process.env.DB_PASSWORD, // ⭐
-  database: process.env.DB_NAME,  // ⭐
-  port: process.env.DB_PORT || 3306,
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME, 
+//   port: process.env.DB_PORT || 3306,
+// });
+
+// // 연결 
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('MYSQL 연결 실패 :', err);
+//     return;
+//   }
+//   console.log('MYSQL 연결 성공');
+// });
+
+// module.exports = connection;
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 10,
+  waitForConnections: true,
 });
 
-// 연결 
-connection.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
-    console.error('MYSQL 연결 실패 :', err);
+    console.error("MYSQL 연결 실패:", err);
     return;
   }
-  console.log('MYSQL 연결 성공');
+
+  console.log("MYSQL 연결 성공");
+  connection.release();
 });
 
-module.exports = connection;
+module.exports = db;
